64c26cf556f09002022f801970a63328395e9094
luksoral <andron2544@mail.ru>
2017-01-19 16:12:57 +0300

    fix

--- app/src/main/java/ru/cproject/vesnaandroid/adapters/MiniStocksAdapter.java
+++ app/src/main/java/ru/cproject/vesnaandroid/adapters/MiniStocksAdapter.java
@@ -47,12 +47,6 @@ public class MiniStocksAdapter extends StocksAdapter {
             return null;
     }
 
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        super.onBindViewHolder(holder, position);
-        ((StockViewHolder) holder).like.setVisibility(View.GONE);
-    }
-
     /*
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
